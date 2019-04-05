ab1b574cbcb248730e7af95264ff4727516b59ed
saisandeep97 <naraparajusaisandeep@gmail.com>
2019-04-05 22:19:11 +0530

    Update 8

--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import subprocess
+import time
 
 
 if __name__ == "__main__": 
@@ -6,4 +7,8 @@ if __name__ == "__main__":
     string1="cd Chat-bot & app.py"
     string2="npm start"
     sts = subprocess.Popen(string1, shell=True)
-    sts2= subprocess.Popen(string2, shell=True)  
+    sts2= subprocess.Popen(string2, shell=True) 
+    time.sleep(5)
+    Call_URL = "http://localhost:3000"
+    mycmd = r'start chrome /new-tab {}'.format(Call_URL)
+    sts3=subprocess.Popen(mycmd,shell = True)  
