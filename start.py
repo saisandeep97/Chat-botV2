bb5e0cf98ecfea0dbd5d8ae0b5bf3e06973630b4
saisandeep97 <naraparajusaisandeep@gmail.com>
2019-04-05 22:12:51 +0530

    Update-5

--- start.py
+++ start.py
@@ -1,7 +1,9 @@
-import threading 
-import os
+import subprocess
+
 
 if __name__ == "__main__": 
-
- os.system("cd Chat-bot & app.py")
-  
+#        # creating threads 
+    string1="cd Chat-bot & app.py"
+    string2="npm start"
+    sts = subprocess.Popen(string1, shell=True)
+    sts2= subprocess.Popen(string2, shell=True)  
